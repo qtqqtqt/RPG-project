8b57cd6c86b69e2ac19f7f167b5e05a661840787
qtqqtqt <violetfantasy0@gmail.com>
2022-09-26 20:32:11 +0300

    adding cinematic

---
+++ Assets/Scripts/Cinematics/CinematicControlsRemover.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Control;
+
+
+namespace RPG.Cinematics
+{
+    public class CinematicControlsRemover : MonoBehaviour
+    {
+        PlayableDirector playableDirector;
+        GameObject player;
+
+        private void Start()
+        {
+            playableDirector = GetComponent<PlayableDirector>();
+            playableDirector.played += DisableControl;
+            playableDirector.stopped += EnableControl;
+
+            player = GameObject.FindGameObjectWithTag("Player");
+
+        }
+
+        void DisableControl(PlayableDirector pd)
+        {
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
+            Debug.Log("DisabledControls");
+        }
+
+        void EnableControl(PlayableDirector pd)
+        {
+            player.GetComponent<PlayerController>().enabled = true;
+            Debug.Log("EnabledControls");
+        }
+    }
+}
---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private bool alreadyTriggered = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player") && !alreadyTriggered)
+            {
+                alreadyTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+            }
+        }
+
+    }
+}
