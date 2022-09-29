1b8aa0e3decb17883ed94fd07fd555ae32198084
qtqqtqt <violetfantasy0@gmail.com>
2022-09-29 09:52:39 +0300

    scene switch

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        enum DestinationPoint
+        {
+            A,B,C,D
+        }
+
+        [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationPoint destination;
+        [SerializeField] float fadeOutTime = 1f;
+        [SerializeField] float fadeInTime = 1f;
+        [SerializeField] float fadeWaitTime = .5f;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+
+            Fader fader = FindObjectOfType<Fader>();
+
+            yield return fader.FadeOut(fadeOutTime);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            yield return new WaitForSeconds(fadeWaitTime);
+            yield return fader.FadeIn(fadeInTime);
+
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal portal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+
+            player.GetComponent<NavMeshAgent>().Warp(portal.spawnPoint.position);
+            player.transform.rotation = portal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this || portal.destination != destination) continue;
+                return portal;
+            }
+
+            return null;
+        }
+    }
+
+    
+
+}
